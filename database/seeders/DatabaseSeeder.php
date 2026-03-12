9a882f735e6a2fe6ac6405017565c87baf6130d2
Anton Kalny <kalnyanton@gmail.com>
2026-03-12 13:03:44 +0200

    feat(main): Move eloquent models to Infrastructure folder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Infrastructure\Persistence\Eloquent\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
